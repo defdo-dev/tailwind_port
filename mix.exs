def8558668c5dda6e84ed955072ea294d60e25ed
Roberto Estrada <restrada@paridin.com>
2024-03-09 12:46:44 -0600

    bump version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule TailwindPort.MixProject do
   def project do
     [
       app: :tailwind_port,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
